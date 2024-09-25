2357b8bfe56723235beafcad161844f604e671c9
jovany <jovany.velez@gmail.com>
2024-09-24 22:18:27 -0500

    primer commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+
+    let miTexto: HTMLInputElement;
+    let cambio = false;
+
+    $effect(() => {
+        miTexto = document.getElementById('miinput') as HTMLInputElement;
+    })
+
+    function cambiar () {
+        cambio ? miTexto.classList.remove('colorFondo'): miTexto.classList.add('colorFondo');
+
+        cambio = !cambio;
+        
+    }
+
+</script>
+
+<form>
+    <button onclick={()=> cambiar()}>Cambiar fondo</button>
+    <input type="text" placeholder="pon tu texto acá" class="colorFondo miClase" id="miinput">
+</form>
+
+<style>
+    .colorFondo {
+        background-color: aquamarine;
+    }
+
+    .miClase {
+        margin: 10px 0;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 20px;
+    }
+
+</style>
